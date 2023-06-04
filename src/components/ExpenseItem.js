49187936c27d87910498a847f3e30507cb16c490
Mahadi Hasan <mahadi.diu791@gmail.com>
2023-06-05 02:03:49 +0600

    updated

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,20 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
     return (
+
         <div className="card">
             <div className="card-body">
-                <div>March 28</div>
-                <div>
-                    <h2>Car Insurance</h2>
-                    <div>$2444</div>
+                <div className="row">
+                    <div className="col-md-2">
+                        March 28
+                    </div>
+                    <div className="col-md-8">
+                        Car Insurance
+                    </div>
+                    <div className="col-md-2">
+                        <h2>$2444</h2>
+                    </div>
                 </div>
             </div>
         </div>
